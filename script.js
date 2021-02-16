db94a86694c926b127e16804d93a2ea2e44bf56d
Colin Zhou <colin.cosun@gmail.com>
2021-02-16 12:49:41 -0800

    commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var movingBtn = document.querySelectorAll('.movingBtn');
 var listOfPanels = document.querySelectorAll('.achivPanel');
-//var firstPanel = document.querySelector('#panel-1');
-//var secondPanel = document.querySelector('#panel-2');
 
 let firstClick = false;
 for (let i = 0; i < movingBtn.length; i++) { //initial click, the first panel gets hidden
@@ -17,13 +15,4 @@ for (let i = 0; i < movingBtn.length; i++) { //initial click, the first panel ge
   })
 }
 
-// movingBtn[1].addEventListener('click', function() { //first panel
-//   firstPanel.setAttribute("hidden", "true");
-//   secondPanel.removeAttribute("hidden")
-// })
-
-// movingBtn[0].addEventListener('click', function() { //first panel
-//   // firstPanel.setAttribute("hidden", "false");
-//   firstPanel.removeAttribute("hidden");
-//   console.log("test")
-// })
+var hiddenPic = document.querySelector()
