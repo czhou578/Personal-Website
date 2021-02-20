ef115041edb5b4276ad4c6e687cd18ff093ac303
Colin Zhou <colin.cosun@gmail.com>
2021-02-19 19:37:25 -0800

    commit

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 var movingBtn = document.querySelectorAll('.movingBtn');
+movingBtn[0].style.backgroundColor = "darkGreen";
+console.log(movingBtn)
 var listOfPanels = document.querySelectorAll('.achivPanel');
 
 let firstClick = false;
 for (let i = 0; i < movingBtn.length; i++) { //initial click, the first panel gets hidden
   firstClick = true;
-
   movingBtn[i].addEventListener('click', function() {
     listOfPanels[i].removeAttribute("hidden");
+    movingBtn[i].style.backgroundColor = 'darkGreen';
     for (let j = 0; j < movingBtn.length; j++) {
       if (j != i) {
         listOfPanels[j].setAttribute("hidden", "true");
+        movingBtn[j].style.backgroundColor = 'transparent';
+
       } 
     }
   })
 }
 
-// $("#hiddenFirst").hide();
-// setInterval(function() {
-//   $(".project-image-slideshow")
-//     .fadeOut(2000)
-//     .next()
-//     .fadeOut(2000)
-//     .next()
-//     .end()
-//     .appendTo(".project-image-slideshow")
-// }, 4000)
