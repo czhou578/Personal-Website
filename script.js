f21d37747ce0242349d3510a41fbb13aafc3473b
= <colizu2020@gmail.com>
2021-05-04 21:46:32 -0700

    commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
+
 var movingBtn = document.querySelectorAll('.movingBtn');
 movingBtn[0].style.backgroundColor = "darkGreen";
-console.log(movingBtn)
+// console.log(movingBtn)
 var listOfPanels = document.querySelectorAll('.achivPanel');
 
 let firstClick = false;
@@ -21,7 +23,6 @@ for (let i = 0; i < movingBtn.length; i++) { //initial click, the first panel ge
 }
 
 const progressLine = document.querySelector('#connect-line')
-console.log(document.activeElement)
 
 window.addEventListener("scroll", function(event) {
   var top = this.scrollY,
@@ -30,13 +31,28 @@ window.addEventListener("scroll", function(event) {
     progressLine.removeAttribute('fill')
     progressLine.setAttribute('fill', "url(#left-to-right)")
   }
-  console.log(top)
 });
-// if (isFocused) {
-//   console.log('true')
-//   progressLine.removeAttribute('fill')
-//   progressLine.setAttribute('fill', 'url(#left-to-right)')
-// }
+
+gsap.registerPlugin(MotionPathPlugin);
+
+MotionPathPlugin.convertToPath('ellipse, line')
+
+gsap.to('#ellipse', {
+  motionPath: {
+    path: "#path",
+    start: 1,
+    end: 0,
+    align: "#path",
+    alignOrigin: [0.5, 0.5],
+    fill: "4DAF4C"
+  },
+  duration: 7,
+  ease: "power1.inOut"
+})
+
+
+
+
 
 
 
