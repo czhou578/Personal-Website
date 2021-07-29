dcfd4b16b4c752a110ed9bf1af3a11eec202d21e
= <colizu2020@gmail.com>
2021-07-29 15:50:16 -0700

    commit

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 
 
 var movingBtn = document.querySelectorAll('.movingBtn');
-movingBtn[0].style.backgroundColor = "darkGreen";
-// console.log(movingBtn)
+movingBtn[0].style.backgroundColor = "#FFDAB9";
 var listOfPanels = document.querySelectorAll('.achivPanel');
 
 let firstClick = false;
 for (let i = 0; i < movingBtn.length; i++) { //initial click, the first panel gets hidden
   firstClick = true;
   movingBtn[i].addEventListener('click', function() {
-    console.log('hellow')
     listOfPanels[i].removeAttribute("hidden");
 
-    movingBtn[i].style.backgroundColor = 'darkGreen';
+    movingBtn[i].style.backgroundColor = '#FFDAB9';
     for (let j = 0; j < movingBtn.length; j++) {
       if (j != i) {
         listOfPanels[j].setAttribute("hidden", "true");
